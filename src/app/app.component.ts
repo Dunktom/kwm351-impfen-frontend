113f317a7d8238b4b5e8f95586437b30f6e8892c
Dunktom <raffetzeder@gmail.com>
2021-05-23 20:38:26 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { AuthenticationService } from './shared/authentication.service';
-import { Location } from './shared/location';
 
 @Component({
   selector: 'impfen-root',
